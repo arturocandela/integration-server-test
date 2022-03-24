2c732a93c2be1bbc2e48201017a08e5af76d181b
Arturo Candela <acandela@cipfpbatoi.es>
2022-03-24 16:13:34 +0100

    Añadida primera versión de la documentación muy básica solo para probar

--- src/main/java/es/devtest/mvn/Autor.java
+++ src/main/java/es/devtest/mvn/Autor.java
@@ -1,5 +1,14 @@
 package es.devtest.mvn;
 
+/**
+ *
+ * Class Author, models a Person
+ * <p>
+ * This class is a POO
+ * 
+ * @author arturo
+ *
+ */
 public class Autor extends Persona {
 	
 	public Autor(String nombre, String apellido) {
