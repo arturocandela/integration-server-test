e913c574bc4337c7d90d9991c0331172e36eaeb8
Arturo Candela <acandela@cipfpbatoi.es>
2022-05-04 18:43:42 +0200

    Implementadas las operaciones

--- src/main/java/dam/proves/MyCalculator.java
+++ src/main/java/dam/proves/MyCalculator.java
@@ -3,18 +3,38 @@ package dam.proves;
 public class MyCalculator {
 
      public int add( int a , int b ){
+
+         if ( a < 0 || b < 0) {
+             throw new IllegalArgumentException();
+         }
+
         return a + b;
     }
 
     public int sub( int a, int b){
+
+        if ( a < 0 || b < 0) {
+            throw new IllegalArgumentException();
+        }
+
         return a - b;
     }
 
     public int mult( int a, int b){
+
+        if ( a < 0 || b < 0) {
+            throw new IllegalArgumentException();
+        }
+
         return a * b;
     }
 
     public int div( int a, int b){
+
+        if ( a < 0 || b < 0) {
+            throw new IllegalArgumentException();
+        }
+
         return a / b;
     }
 
